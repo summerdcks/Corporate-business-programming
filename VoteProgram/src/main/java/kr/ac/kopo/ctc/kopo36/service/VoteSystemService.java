4842d04fbef564561e18e8a267bef0825ed26baf
summerdcks <131256051+summerdcks@users.noreply.github.com>
2023-06-14 15:33:46 +0900

    Delete VoteProgram directory

--- VoteProgram/src/main/java/kr/ac/kopo/ctc/kopo36/service/VoteSystemService.java
+++
@@ -1,8 +0,0 @@
-package kr.ac.kopo.ctc.kopo36.service;
-
-public interface VoteSystemService {
-	int getAvailableKiho(); //새로 입력이 가능한 기호를 도출하는 함수
-	int countAll();  //총투표수를 가져오는 함수
-	int selectOne(int kiho, int age); //특정후보의 연령대별 득표수를 가져오기 위한 함수
-	int countVotes(int kiho);  //특정후보의 총득표수를 가져오는 함수
-}
